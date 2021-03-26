de1512f744e68e5bd7171745aa51f91a5f1a9f91
CalumD <github@clumd.com>
2021-03-26 23:20:23 +0000

    Update Factorio default version to 1.10.30, and attempt to avoid the recursive symbolic link to bound directories .

--- Factorio/README.md
+++ Factorio/README.md
@@ -1,6 +1,5 @@
-This Repo is **heavily** inspired from:
+Inspired from:
 https://github.com/factoriotools/factorio-docker
 
-Requires mount from host to: /factorio_files
-Requires mount to port 34197/udp
+Requires mount from host to: /factorio Requires mount to port 34197/udp
 
--- Factorio/startup.sh
+++ Factorio/startup.sh
@@ -1,37 +1,57 @@
 #!/bin/bash
 
-mkdir -p $GAME_CONFIG_DIR
 mkdir -p $GAME_SAVE_DIR
+mkdir -p $GAME_CONFIG_DIR
 mkdir -p $GAME_MOD_DIR
 
 # Get the game
-wget -qc https://factorio.com/get-download/$GAME_VERSION/headless/linux64 -O - | tar -xJ -C /opt
-ln -s "$GAME_SAVE_DIR" /opt/factorio/saves
-ln -s "$GAME_MOD_DIR" /opt/factorio/mods
+wget -qc https://factorio.com/get-download/$GAME_VERSION/headless/linux64 -O - | tar -xJ -C /tmp
+ln -s "$GAME_SAVE_DIR" /tmp/factorio/saves
+ln -s "$GAME_MOD_DIR" /tmp/factorio/mods
 
 # Check files exist.
 if [[ ! -f $GAME_CONFIG_DIR/server-settings.json ]]; then
-  cp /opt/factorio/data/server-settings.example.json "$GAME_CONFIG_DIR/server-settings.json"
+  cp /tmp/factorio/data/server-settings.example.json "$GAME_CONFIG_DIR/server-settings.json"
+fi
+
+if [[ ! -f $GAME_CONFIG_DIR/map-gen-settings.json ]]; then
+  cp /tmp/factorio/data/map-gen-settings.example.json "$GAME_CONFIG_DIR/map-gen-settings.json"
+fi
+
+if [[ ! -f $GAME_CONFIG_DIR/map-settings.json ]]; then
+  cp /tmp/factorio/data/map-settings.example.json "$GAME_CONFIG_DIR/map-settings.json"
 fi
 
 if [[ ! -f $GAME_CONFIG_DIR/server-adminlist.json ]]; then
-  touch "$GAME_CONFIG_DIR/server-adminlist.json"
+  echo "[]" >"$GAME_CONFIG_DIR/server-adminlist.json"
+fi
+
+if [[ ! -f $GAME_CONFIG_DIR/server-banlist.json ]]; then
+  echo "[]" >"$GAME_CONFIG_DIR/server-banlist.json"
 fi
 
 # Delete incomplete saves (such as after a forced exit)
-INCOMPLETE_SAVES=$( find -L "$GAME_SAVE_DIR" -iname \*.tmp.zip -mindepth 1 | wc -l )
+INCOMPLETE_SAVES=$(find -L "$GAME_SAVE_DIR" -mindepth 1 -iname \*.tmp.zip | wc -l)
 if [[ $INCOMPLETE_SAVES -gt 0 ]]; then
   rm -f "$GAME_SAVE_DIR"/*.tmp.zip
 fi
 
-# Run the Game
-exec /opt/factorio/bin/x64/factorio \
+TOTAL_SAVES=$(find -L "$GAME_SAVE_DIR" -mindepth 1 -iname \*.zip | wc -l)
+if [[ $TOTAL_SAVES == 0 ]]; then
+  /tmp/factorio/bin/x64/factorio \
+    --create "$GAME_SAVE_DIR/_autosave1.zip" \
+    --map-gen-settings "$GAME_CONFIG_DIR/map-gen-settings.json" \
+    --map-settings "$GAME_CONFIG_DIR/map-settings.json"
+fi
+
+## Run the Game
+exec /tmp/factorio/bin/x64/factorio \
   --port "$GAME_PORT" \
   --server-settings "$GAME_CONFIG_DIR/server-settings.json" \
+  --server-banlist "$GAME_CONFIG_DIR/server-banlist.json" \
   --server-adminlist "$GAME_CONFIG_DIR/server-adminlist.json" \
   --console-log "$GAME_CONFIG_DIR/serverlog.log" \
   --server-id "$GAME_CONFIG_DIR/serverID.json" \
   --start-server-load-latest
 
-
-
+#/bin/bash
